46eef3384f38484f2b14dbc1ef2ecf2f6d4b883d
Rwanda S <rwanda.ace@gmail.com>
2020-06-21 17:17:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,6 +61,13 @@ class Student
     DB[:conn].execute(sql)
   end
   def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade  = ?
+    SQL
+    DB[:conn].execute(sql, 9).map {
+      |row|
+      self.new_from_db(row)
+    }
   end
   def self.students_below_12th_grade
   end
